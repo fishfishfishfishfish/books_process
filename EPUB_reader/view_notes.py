5bf606de002444d9f4855fc95c28399473728f4b
fishfishfishfishfish <619156737@qq.com>
2019-07-15 23:31:43 +0800

    highlight label

--- EPUB_reader/view_notes.py
+++ EPUB_reader/view_notes.py
@@ -12,6 +12,7 @@ class TKContext:
         self.window = tkinter.Tk()
         self.db_name = tkinter.StringVar()
         self.cur_highlight = tkinter.StringVar()
+        self.cur_highlight_id = 1
         # 选取文件路径
         self.db_chose_btn = tkinter.Button(self.window, text="chose db", command=self.get_db_name)
         self.db_name_label = ttk.Label(self.window, textvariable=self.db_name,
@@ -24,8 +25,10 @@ class TKContext:
                                               selectmode="browse", height=28)
         self.content_vbar = ttk.Scrollbar(self.window, orient=tkinter.VERTICAL, command=self.content_tree_view.yview)
         # 显示当前标记
-        self.highlight_label = ttk.Label(self.window, textvariable=self.cur_highlight,
-                                         justify="left")
+        self.highlight_label = ttk.Label(self.window, textvariable=self.cur_highlight, justify="left")
+        self.last_highlight_btn = tkinter.Button(self.window, text="last", command=self.last_highlight)
+        self.next_highlight_btn = tkinter.Button(self.window, text="next", command=self.next_highlight)
+
     def config_wigets(self):
         # 图书内容列表
         self.content_tree_view.column("1", width=500, anchor="w")
@@ -41,12 +44,17 @@ class TKContext:
         self.db_chose_btn.grid(row=1, column=1, padx=10, pady=10, sticky="ew")
         self.db_name_label.grid(row=1, column=2, columnspan=10, padx=10, pady=10, sticky="ew")
         # 图书内容列表
-        self.content_tree_view.grid(row=3, column=1, columnspan=6, pady=10, sticky="e")
-        self.content_vbar.grid(row=3, column=7, pady=10, sticky="nsw")
+        self.content_tree_view.grid(row=3, column=1, columnspan=6, rowspan=3, pady=10, sticky="e")
+        self.content_vbar.grid(row=3, column=7, pady=10, rowspan=3, sticky="nsw")
+        # 显示当前标记
+        self.highlight_label.grid(row=3, column=9, columnspan=3, pady=10, sticky="w")
+        self.last_highlight_btn.grid(row=4, column=9, columnspan=3, pady=10, sticky="nw")
+        self.next_highlight_btn.grid(row=5, column=9, columnspan=3, pady=10, sticky="nw")
 
     def get_db_name(self):
         self.db_name.set(filedialog.askopenfilename())
         self.get_book_content()
+        self.cur_highlight.set(self.get_first_highlight()[1])
 
     def get_book_content(self):
         for item in self.content_tree_view.get_children():
@@ -63,6 +71,29 @@ class TKContext:
             for item in cur:
                 self.content_tree_view.insert("", "end", values=[item[1], item[2]], iid=item[0], tags=[f"type{tag}"])
                 tag = (tag + 1) % 2
+
+    def get_first_highlight(self):
+        with utils_sqlite.sqlite_shell(self.db_name.get()) as cur:
+            cur.execute("""
+                        SELECT id, highlight FROM highlight where id = 1;
+                        """)
+            return cur.fetchone()
+
+    def last_highlight(self):
+        self.cur_highlight_id = self.cur_highlight_id - 1 if self.cur_highlight_id > 1 else 1
+        with utils_sqlite.sqlite_shell(self.db_name.get()) as cur:
+            cur.execute(f"""
+                        SELECT id, highlight FROM highlight where id = {self.cur_highlight_id};
+                        """)
+            self.cur_highlight.set(cur.fetchone()[1])
+
+    def next_highlight(self):
+        self.cur_highlight_id = self.cur_highlight_id + 1
+        with utils_sqlite.sqlite_shell(self.db_name.get()) as cur:
+            cur.execute(f"""
+                        SELECT id, highlight FROM highlight where id = {self.cur_highlight_id};
+                        """)
+            self.cur_highlight.set(cur.fetchone()[1])
 
 
 if __name__ == "__main__":
