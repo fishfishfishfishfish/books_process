95f0c326ac532829ed8bc8669b33720a415ca816
fishfishfishfishfish <619156737@qq.com>
2019-07-26 00:00:58 +0800

    接下来：be选择db文件，before_catch写入db文件

--- clipDict/get_meaning.py
+++ clipDict/get_meaning.py
@@ -1,74 +1,22 @@
-import requests
-import sqlite3
-import random
-import time
-import socket
-import http.client
+import re
+from urllib.request import urlopen
+from urllib.parse import quote
 from bs4 import BeautifulSoup
+from lxml.html import etree as et
+
+import utils_text_preprocess
 
 
-def get_content(url):
-    header = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-        'Accept-Encoding': 'gzip, deflate',
-        'Accept-Language': 'zh-CN,zh;q=0.9',
-        'Cache - Control': 'max - age = 0',
-        'Connection': 'keep-alive',
-        'Host': 'www.iciba.com',
-        'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'
-    }
-    timeout = random.choice(range(80, 180))
-    while True:
-        try:
-            rep = requests.get(url, headers=header, timeout=timeout)
-            rep.encoding = 'utf-8'
-            break
-        except socket.timeout as e:
-            print('3:', e)
-            time.sleep(random.choice(range(8, 15)))
-
-        except socket.error as e:
-            print('4:', e)
-            time.sleep(random.choice(range(20, 60)))
-
-        except http.client.BadStatusLine as e:
-            print('5:', e)
-            time.sleep(random.choice(range(30, 80)))
-
-        except http.client.IncompleteRead as e:
-            print('6:', e)
-            time.sleep(random.choice(range(5, 15)))
-
-    return rep.text  # return html_text
-
-
-def get_mean(word):
-    res = ""
-    soup = BeautifulSoup(get_content('http://www.iciba.com/' + word), "html.parser")
-    body = soup.body
-    try:
-        contents = body.find('div', class_="screen")
-        contents = contents.find('div', class_="container")
-        contents = contents.find('div', class_="container-left")
-        contents = contents.find('div', class_="js-base-info")
-        contents = contents.find('div', class_='info-article info-base')
-        contents = contents.find('div', class_='in-base')
-        if contents.find('ul', class_='') is None:
-            contents = contents.div.div
-            res = contents.get_text(strip=True)
-        else:
-            contents = contents.find('ul')
-            for li in contents.find_all('li'):
-                res = res + li.get_text(strip=True) + "<br>"
-    except BaseException as e:
-        print('when getting \'' + word + '\'meanings, ', e)
-        # return 'no meaning'
-        return 'NA'
-
-    if contents is None:
-        print('when getting \'' + word + '\'meanings, no meaning can be found.')
-        return 'NA'
-
-    return res
+def from_youdao(query: str):
+    html = urlopen("https://www.youdao.com/w/eng/%s/#keyfrom=dict2.index" % (quote(query), ))
+    bs_obj = BeautifulSoup(html, 'html.parser')
+    html_str = bs_obj.prettify()
+    html_str = re.split("(</html>)", html_str)
+    html_str = html_str[0] + html_str[1]
+    root = et.fromstring(html_str)
+    ns = {"default": root.nsmap[None]}
+    xpath = ".//*[contains(@id, 'phrsListTab')]//*[contains(@class, 'container')]//default:li//text()"
+    li_text = root.xpath(xpath, namespaces=ns)
+    meaning = [utils_text_preprocess.clean_text(li) for li in li_text]
+    return meaning
 
--- clipDict/view.py
+++ clipDict/view.py
@@ -1,6 +1,7 @@
 import threading
 import pyperclip
 import tkinter
+import tkinter.messagebox
 from tkinter import ttk
 
 import get_meaning
@@ -12,32 +13,57 @@ class TKContext:
         self.window = tkinter.Tk()
         self.last_query = ""
         self.cur_query_str = tkinter.StringVar()
-        self.query_label = ttk.Label(self.window, textvariable=self.cur_query_str,
-                                     width=50, justify="left",  # 文本对齐方式
-                                     background="white",  # 背景颜色
-                                     )
+        self.query_content_disp = tkinter.Text(self.window, height=5)
+        self.cur_query_word = tkinter.StringVar()
+        self.word_disp = ttk.Label(self.window, textvariable=self.cur_query_word, width=30)
+        self.cur_meaning = tkinter.StringVar()
+        self.meaning_choosing_label = ttk.Label(self.window, text="meanings: ", justify="left")
+        self.meaning_choosing = ttk.Combobox(width=30, height=5, values=[], textvariable=self.cur_meaning)
         self.confirm_btn = tkinter.Button(self.window, text="start catch", command=self.create_thread)
+        self.get_word_btn = tkinter.Button(self.window, text="get word", command=self.get_word)
 
     def grid_wigets(self):
-        self.query_label.grid(row=1, column=1, columnspan=5)
-        self.confirm_btn.grid(row=2, column=1)
+        self.query_content_disp.grid(row=1, column=1, columnspan=3)
+        self.get_word_btn.grid(row=2, column=2)
+        self.word_disp.grid(row=3, column=1)
+        self.meaning_choosing.grid(row=3, column=2)
+        self.confirm_btn.grid(row=4, column=2)
+        self.create_thread()
+
+    def before_catch(self):
+        self.confirm_btn["state"] = tkinter.DISABLED
+        self.get_word_btn["state"] = tkinter.DISABLED
+
+    def after_catch(self):
+        self.confirm_btn["state"] = tkinter.NORMAL
+        self.get_word_btn["state"] = tkinter.NORMAL
+        self.query_content_disp.delete("1.0", tkinter.END)
+        self.query_content_disp.insert("1.0", self.cur_query_str.get())
 
     def catch_query(self):
-        self.confirm_btn["state"] = tkinter.DISABLED
+        self.before_catch()
         while True:
             curr = pyperclip.paste()
             if curr != self.last_query:
                 self.last_query = curr
                 good_curr = curr.strip()
-                self.cur_query_str.set(good_curr)
-                break
-        self.confirm_btn["state"] = tkinter.NORMAL
+                if tkinter.messagebox.askyesno(title='catched', message=f'{good_curr}, record it?'):
+                    self.cur_query_str.set(good_curr)
+                    break
+        self.after_catch()
 
     # Running methods in Threads
     def create_thread(self):
         run_thread = threading.Thread(target=self.catch_query)
         print(run_thread)
         run_thread.start()
+
+    def get_word(self):
+        self.cur_query_word.set(self.query_content_disp.selection_get())
+        self.meaning_choosing.config(values=self.get_cur_meanings())
+
+    def get_cur_meanings(self):
+        return get_meaning.from_youdao(self.cur_query_word.get())
 
 
 if __name__ == "__main__":
